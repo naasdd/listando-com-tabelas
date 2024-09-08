b94e0ef8a48d1afabe8b42899dec5bb3631fa02c
José Gabriel Santos Gomes <70996909+naasdd@users.noreply.github.com>
2024-09-07 22:22:57 -0300

    axyhkglno´jh

--- front/listarEntregas.js
+++ front/listarEntregas.js
@@ -1,17 +1,17 @@
-let acagetElementById('acao')
-let tbody
+let acao = document.getElementById('acao')
+let tbody = document.querySelector('tbody')
 
 acao.addEventListener('click', () => {
 
     fetch('http://localhost:3000/entregas')
-    .then(response => response.json())
-    .then(valores => {
-        let dados = JSON.stringify(valores)
-        console.log(dados)
-        res.innerHTML = ''
-        valores.forEach(atual => {
+        .then(response => response.json())
+        .then(valores => {
+            let dados = JSON.stringify(valores)
+            console.log(dados)
+            tbody.innerHTML = ''
+            valores.forEach(atual => {
+                tbody.innerHTML += `<tr><td>${atual.codEntrega}</td><td>${atual.local}</td><td>${atual.data}</td>`
 
-            res.innerHTML += `codEntrega: ${atual.codEntrega}<br> Local: ${atual.local} <br> data: ${atual.data}`
-        });
-    })
+            });
+        })
 })
